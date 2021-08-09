9dc43a7e804531e784d5b07cdafae906d5794837
hsantos97 <58005145+hsantos97@users.noreply.github.com>
2021-08-09 13:44:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,32 @@
-#define uart_base 0x10000000
-
-static void putc(char c) {
-    
-    volatile char *reg = (char *) uart_base;
-    *(reg) = c;
-}
+#include "types.h"
+#include "defs.h"
 
 void puts(char *s) {
-    while (*s) putc(*s++);
-    putc('\n');
+    while (*s) uartputc(*s++);
+    uartputc('\n');
 }
 
-void entry() {
-    puts("- Ola mundo! (disse o kernel)");
+int
+entry() {
+    // \uXXXX e \UXXXXXXXX são chamados universal-character-name
+    puts("\u26F0  ROS - Roncador Operating System (V0.1) \U0001F920");
+    int c;
+    for(;;) {
+        c = uartgetc();
+        if (c == -1)
+            continue;
+        switch(c) {
+            case 127:
+                uartputc('\b');
+                uartputc(' ');
+                uartputc('\b');
+                break;
+            case '\r':
+                puts("\r\n");
+                break;
+            default:
+                uartputc(c);
+        }
+    }
+    return 0;
 }
